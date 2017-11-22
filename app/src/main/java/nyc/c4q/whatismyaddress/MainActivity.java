203d69abd83c4eac46c7e640204b3fbd45b92be7
Taishawn Williams <twill4317@withs.org>
2017-11-21 20:06:39 -0500

    part01 commit

--- app/src/main/java/nyc/c4q/whatismyaddress/MainActivity.java
+++ app/src/main/java/nyc/c4q/whatismyaddress/MainActivity.java
@@ -1,13 +1,43 @@
 package nyc.c4q.whatismyaddress;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText userEmail;
+    Button saveButton,nextButton;
+    SharedPreferences preferences;
+    SharedPreferences.Editor editor;
+    String key = "address_shared_preferences";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        preferences = getSharedPreferences(key,MODE_PRIVATE);
+        editor = preferences.edit();
+
+
+        userEmail = findViewById(R.id.email_field);
+        saveButton = findViewById(R.id.save_button);
+        nextButton = findViewById(R.id.next_button);
+    }
+
+    public void saveEmailAdd(View view){
+        String emailAddress = userEmail.getText().toString();
+        editor.putString(emailAddress,emailAddress);
+        userEmail.setText("");
+    }
+
+    public void nextIntent(View view){
+        Intent toRecycler = new Intent(this, RecyclerActivity.class);
+        toRecycler.putExtra("sharedpref",key);
+        startActivity(toRecycler);
     }
 }
